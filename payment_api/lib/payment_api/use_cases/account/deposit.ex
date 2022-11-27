3f2a48ae0377250f1eadef884dcd74442f9dced9
Vitor Hugo <vitorzcomputacao@gmail.com>
2022-11-27 13:58:31 +0000

    add usecase for account.deposit

---
+++ payment_api/lib/payment_api/use_cases/account/deposit.ex
@@ -0,0 +1,17 @@
+defmodule PaymentApi.UseCases.Account.Operation do
+  alias PaymentApi.UseCases.Account.Operation
+  alias PaymentApi.Repo
+
+  def call(params) do
+    params
+    |> Operation.call(:deposit)
+    |> run_transaction()
+  end
+
+  defp run_transaction(multi) do
+    case Repo.transaction(multi) do
+      {:error, _operation, reason, _changes} -> {:error, reason}
+      {:ok, %{deposit: account}} -> {:ok, account}
+    end
+  end
+end
