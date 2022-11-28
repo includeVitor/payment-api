666d2e11d6c3f289f972bb7687f0140c875bdd86
Vitor Hugo <vitorzcomputacao@gmail.com>
2022-11-28 01:25:46 +0000

    add test for account transaction

--- payment_api/test/payment_api_web/controllers/account_controller_test.exs
+++ payment_api/test/payment_api_web/controllers/account_controller_test.exs
@@ -93,4 +93,70 @@ defmodule PaymentApiWeb.AccountControllerTest do
       assert expected_response == response
     end
   end
+
+  describe "transaction" do
+    setup %{conn: conn} do
+      params_from_user = %{
+        nickname: "includevitor",
+        email: "includevitor@email.com"
+      }
+
+      {:ok, %User{account: %Account{id: from}}} = PaymentApi.create_user(params_from_user)
+
+      PaymentApi.deposit(%{"id" => from, "value" => "50.00"})
+
+      params_to_user = %{
+        nickname: "includedaniel",
+        email: "includedaniel@email.com"
+      }
+
+      {:ok, %User{account: %Account{id: to}}} = PaymentApi.create_user(params_to_user)
+
+      PaymentApi.deposit(%{"id" => to, "value" => "50.00"})
+
+      {:ok, conn: conn, from: from, to: to}
+    end
+
+    test "should be able to make an transaction", %{conn: conn, from: from, to: to} do
+      params = %{"from" => from, "to" => to, "value" => "25.04"}
+
+      response =
+        conn
+        |> patch(Routes.account_path(conn, :transaction, params))
+        |> json_response(:ok)
+
+      expected_response = %{
+        "account" => %{
+          "from_account" => %{
+            "balance" => "24.96",
+            "id" => from
+          },
+          "to_account" => %{
+            "balance" => "75.04",
+            "id" => to
+          }
+        },
+        "message" => "Transaction processed successfully"
+      }
+
+      assert expected_response === response
+    end
+
+    test "should be able to handle invalid params while making an transaction", %{
+      conn: conn,
+      from: from,
+      to: to
+    } do
+      params = %{"from" => from, "to" => to, "value" => "not an number"}
+
+      response =
+        conn
+        |> patch(Routes.account_path(conn, :transaction, params))
+        |> json_response(:bad_request)
+
+      expected_response = %{"message" => "Invalid value!"}
+
+      assert expected_response == response
+    end
+  end
 end
