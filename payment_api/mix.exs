66f2f9a876be7ddda379ed475b364b4e121c1681
Vitor Hugo <vitorzcomputacao@gmail.com>
2022-11-29 02:28:30 +0000

    publish to codecov

--- payment_api/mix.exs
+++ payment_api/mix.exs
@@ -16,7 +16,8 @@ defmodule PaymentApi.MixProject do
         coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
-        "coveralls.html": :test
+        "coveralls.html": :test,
+        "coveralls.json": :test
       ]
     ]
   end
